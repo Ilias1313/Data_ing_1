175d1a44e0c56f3aa893c64589959603e6de9880
ilias <iliasdu1313@gmail.com>
2024-11-08 17:27:18 +0100

    Etape deux, prix decroissant

---
+++ dags/fetch_and_preview2.py
@@ -0,0 +1,62 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
+import json
+
+def get_data(**kwargs):
+    url = 'https://raw.githubusercontent.com/Ilias1313/Data_ing_1/refs/heads/main/dags/data.csv'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        df = pd.read_csv(url, header=None, names=['Category', 'Price', 'Quantity'])
+        
+        # Conversion du DataFrame en chaîne JSON pour XCom
+        json_data = df.to_json(orient='records')
+        kwargs['ti'].xcom_push(key='data', value=json_data)
+    else:
+        raise Exception(f'Failed to get data, HTTP status code: {response.status_code}')
+
+def preview_data(**kwargs):
+    # Récupérer les données JSON à partir de XCom
+    output_data = kwargs['ti'].xcom_pull(key='data', task_ids='get_data')
+    if output_data:
+        output_data = json.loads(output_data)
+    else:
+        raise ValueError('No data received from XCom')
+    
+    # DataFrame depuis les données JSON
+    df = pd.DataFrame(output_data)
+
+    # Trier par ordre décroissant des prix
+    df_sorted = df.sort_values(by='Price', ascending=False)
+
+    # Afficher les résultats
+    print(df_sorted.head(20))  # Afficher les 20 premiers éléments triés
+
+default_args = {
+    'owner': 'datamasterylab.com',
+    'start_date': datetime(2024, 11, 1),
+    'catchup': False
+}
+
+dag = DAG(
+    'fetch_and_preview',
+    default_args=default_args,
+    schedule_interval=timedelta(days=1)
+)
+
+get_data_from_url = PythonOperator(
+    task_id='get_data',
+    python_callable=get_data,
+    dag=dag
+)
+
+preview_data_from_url = PythonOperator(
+    task_id='preview_data',
+    python_callable=preview_data,
+    dag=dag
+)
+
+get_data_from_url >> preview_data_from_url
